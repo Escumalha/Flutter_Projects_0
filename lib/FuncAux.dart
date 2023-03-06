d82cc998ed0a2560b62e0b5df323c745c6708817
Escumalha <breno.golo@gmail.com>
2023-03-06 18:09:37 -0300

    alteração completa

---
+++ lib/FuncAux.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+String TopNavBarBuilder(int index) {
+  switch (index) {
+    case 0:
+      return "Ordenar";
+    case 1:
+      return "Pra entrega";
+    case 2:
+      return "Entrega gratis";
+    default:
+      return "";
+  }
+}
+
+EdgeInsetsGeometry PaddingTopNavBarBuilder(int index) {
+  if (index < 3) {
+    return EdgeInsets.all(8);
+  } else {
+    return EdgeInsets.zero;
+  }
+}
+
+Widget BuildCards(int index) {
+  var img = Image.asset("");
+  switch (index) {
+    case 0:
+      img = Image.asset('assets/images/ImgFoodCards1.png');
+      break;
+    case 1:
+      img = Image.asset('assets/images/ImgFoodCards2.png');
+      break;
+    case 2:
+      img = Image.asset('assets/images/ImgFoodCards3.png');
+      break;
+    default:
+      img = Image.asset("");
+      return Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        child: img,
+      );
+  }
+}
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'FuncAux.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,15 +9,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final list = <String>[];
-  final controller = TextEditingController();
+  String dropDownValue = "Ordenar";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Color.fromRGBO(255, 227, 125, 1),
-      ),
-    );
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              "Cariri Delivery",
+              style: TextStyle(
+                color: Color.fromRGBO(5, 31, 50, 1),
+                fontFamily: 'Inter',
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          elevation: 0,
+          backgroundColor: const Color.fromRGBO(255, 227, 125, 1),
+        ),
+        body: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Expanded(
+            child: Container(
+              height: 100,
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                  maxLines: null,
+                  minLines: null,
+                  expands: true,
+                  decoration: InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      prefixIcon: const Icon(Icons.search, size: 30),
+                      prefixIconColor: const Color.fromRGBO(255, 227, 125, 1),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color.fromRGBO(5, 31, 50, 1), width: 2)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color.fromRGBO(5, 31, 50, 1), width: 2)),
+                      isDense: true,
+                      labelText: "Pesquisar",
+                      labelStyle: const TextStyle(
+                        color: Color.fromRGBO(5, 31, 50, 0.7),
+                        fontFamily: 'Inter',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ))),
+            ),
+          ),
+          ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) => Card(
+              child: SizedBox(
+                height: 200,
+                width: 200,
+                child: Card(
+                  child: InkWell(
+                    child: Image.asset("assets/images/ImgFoodCards1.png"),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ]));
   }
+
+  DropdownMenuItem<String> BuildMenuItem(String item) => DropdownMenuItem(
+        value: item,
+        child: Text(item,
+            style: const TextStyle(
+              color: Color.fromRGBO(255, 227, 125, 1),
+              fontFamily: 'Inter',
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            )),
+      );
 }
